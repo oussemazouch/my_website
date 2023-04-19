57d9f765c2ac6add8c6f4c6eefe5445943ef81ab
oussemazouch <oussemazouch136117@gmail.com>
2023-04-20 00:04:58 +0100

    Added my project

---
+++ data/headerNavLinks.js
@@ -0,0 +1,24 @@
+const headerNavLinks = [
+  {
+    href: '/blog',
+    title: 'Blog',
+  },
+  // {
+  //   href: '/tags',
+  //   title: 'cv',
+  // },
+  {
+    href: 'https://medium.com/@oussemazouch/format-shop-challenge-from-the-jingle-bell-ctf-e7863d3e14b2',
+    title: 'cv',
+  },
+
+  {
+    href: '/projects',
+    title: 'Certifs',
+  },
+  {
+    href: '/about',
+    title: 'About',
+  },
+]
+export default headerNavLinks
---
+++ data/projectsData.js
@@ -0,0 +1,22 @@
+const projectsData = [
+  {
+    title: 'THM:Jr penetration tester',
+    description: `This learning path covers the core technical skills of a junior penetration tester. It gave me  the  necessary skills to perform security assessments against web applications`,
+    imgSrc: '/static/images/THMlogo.png',
+    href: 'https://drive.google.com/file/d/1fuecJiISyQgnr4bCYmNchvZbKNXsA4lp/view',
+  },
+  {
+    title: 'Red Hat System Administration I (RH124)',
+    description: `The course provides me with Linux administration competence by focusing on core administration tasks`,
+    imgSrc: '/static/images/redhat3.jpg',
+    href: 'https://drive.google.com/file/d/1VyfP0UGxcTfrVjpSTd8K-IH_RJ0oQMvz/view',
+  },
+
+  {
+    title: 'pentesterlab: Essential badge ',
+    description: `This badge aims at covering the most common web vulnerabilities.`,
+    imgSrc: '/static/images/pentesterlab.png',
+    href: 'https://ptl-certs.s3.amazonaws.com/PTLE6092.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI5SYYGSB2WKF7OAA%2F20230419%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230419T154657Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=a45267a27dfb7b7e7d74fa985d237ed44d8e390bc90b51dfd57a59504cddba33',
+  },
+]
+export default projectsData
