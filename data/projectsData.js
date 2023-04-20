f1be0f96178f2e535129fb09c13220ba2fede36d
oussemazouch <oussemazouch136117@gmail.com>
2023-04-20 01:21:53 +0100

    second commit

--- data/projectsData.js
+++ data/projectsData.js
@@ -16,7 +16,7 @@ const projectsData = [
     title: 'pentesterlab: Essential badge ',
     description: `This badge aims at covering the most common web vulnerabilities.`,
     imgSrc: '/static/images/pentesterlab.png',
-    href: 'https://ptl-certs.s3.amazonaws.com/PTLE6092.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI5SYYGSB2WKF7OAA%2F20230419%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230419T154657Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=a45267a27dfb7b7e7d74fa985d237ed44d8e390bc90b51dfd57a59504cddba33',
+    href: '/static/images/pen.pdf',
   },
 ]
 export default projectsData
